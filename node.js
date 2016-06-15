81c28e8c62f172cde820217519d8af42634b1c4b
mike.zhang <mike.zhang@yeamoney.cn>
2016-06-15 18:33:22 +0800

    t
    t

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
 console.log("准备删除文件！");
-fs.unlink('input.txt', function(err) {
+fs.unlink('input.txt', function (err) {
     if (err) {
         return console.error(err);
     }
